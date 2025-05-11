7e4cbabe879a55ef6fa11669a5d1d77c5d3b5087
Alexandr <sashka180588@gmail.com>
2025-05-11 10:31:39 +0700

    change files

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+//Конфликты при слиянии веток, при написании разного кода
+//в разных ветках на одинаковых строчках
+
+//master
+
 console.log('Hello world');
 
+//develop
 
+console.log('Hello JavaScript');
 
+//при merg develop -> master
+
+//Accept Current Change(оставить изменения в master)
+//Accept Incoming Change(оставить входящие изменения)
+//Accept Both Changes(оставить (соединить все))
